160a4f9fad5be41934bb96719c59a8e111d65167
Andrea Anzani <andrea.anzani@gmail.com>
2022-01-06 12:49:04 +0100

    tn306 code cleanups

--- src/Extensions/Tn306/knob303.cpp
+++ src/Extensions/Tn306/knob303.cpp
@@ -1,16 +1,9 @@
 #include "knob303.h"
 #include "Utils.h"
 #include "TNTrack.h"
-#include <Window.h>
 #include "TNKnob.h"
 
-const 	rgb_color  	yellow 	={247,255,146,181};
-
-#define	ACCENT		1983
-#define	TIMEDELAY	1987
-
-#define	POT_H		24
-
+#include <Window.h>
 #include <stdio.h>
 #include <Application.h>
 #include <Resources.h>
@@ -23,39 +16,32 @@ TB303Knobs::TB303Knobs(BRect r) : BView(r, "TN306", B_FOLLOW_NONE,B_WILL_DRAW|B_
 	BGridLayout* grid = BLayoutBuilder::Grid<>();
 	SetLayout(grid);
 	
-	grid->SetSpacing(0, 0);
+	grid->SetSpacing(0, 0);	
 	
+	tune  =  MakeKnob("Tune",      &TB303Knobs::do_tune);	
+	dist  =  MakeKnob("Dist",      &TB303Knobs::do_dist);
+	envmod = MakeKnob("EnvMod",    &TB303Knobs::do_envmod);
+	decay  = MakeKnob("Decay",     &TB303Knobs::do_decay);	
+	cutoff = MakeKnob("Cutoff",    &TB303Knobs::do_cutoff);	
+	reso   = MakeKnob("reso",      &TB303Knobs::do_reso);	
+	delay  = MakeKnob("Delay",     &TB303Knobs::do_delay);
+	feedbk = MakeKnob("Feedback",  &TB303Knobs::do_feedbk);
+	tdelay = MakeKnob("TimeDelay", &TB303Knobs::do_timedelay);	
 	
-	tune  =  MakeKnob( "Tune",    &TB303Knobs::do_tune);	
-	dist  =  MakeKnob( "Dist",    &TB303Knobs::do_dist);
-	envmod = MakeKnob("EnvMod",   &TB303Knobs::do_envmod);
-	decay  = MakeKnob("Decay",    &TB303Knobs::do_decay);	
-	cutoff = MakeKnob("Cutoff",   &TB303Knobs::do_cutoff);	
-	reso   = MakeKnob("reso",     &TB303Knobs::do_reso);	
-	delay  = MakeKnob("Delay",    &TB303Knobs::do_delay);
-	feedbk = MakeKnob("Feedback", &TB303Knobs::do_feedbk);
-	tdelay = MakeKnob("TimeDelay", &TB303Knobs::do_timedelay);
-	
-	
-	//manca tune
 	grid->AddView(tune, 	0, 0);
-	grid->AddView(dist, 	1, 0);
-	
+	grid->AddView(dist, 	1, 0);	
 	grid->AddView(envmod, 	0, 1);	
 	grid->AddView(decay, 	1, 1);
-	grid->AddView(cutoff, 	2, 1);
-	
+	grid->AddView(cutoff, 	2, 1);	
 	grid->AddView(reso, 	0, 2);
 	grid->AddView(delay, 	1, 2);
 	grid->AddView(feedbk, 	2, 2);
-	grid->AddView(tdelay, 	3, 2);
-	
+	grid->AddView(tdelay, 	3, 2);	
 	
 	
 	fTrack = NULL;
 		
-	tune->SetValue(0.5f); //0->tuned
-	
+	tune->SetValue(0.5f); //0->tuned	
 }
 
 void
@@ -66,21 +52,19 @@ TB303Knobs::Reset(TNTrack *track)
 	if(!fTrack)
 		return;
 	
-	vco=&track->vco; vcf=&track->vcf; vca=&track->vca; dly=&track->dly;
-	
-	if (Window()->Lock())
+	if (Window()->Lock()) //?
 	{
-		cutoff->SetValue(vcf->getCutoff());
-		reso->SetValue(vcf->getResonance());
-		envmod->SetValue(vcf->getEnvmod());
-		decay->SetValue(vcf->getDecay());
-	   	 delay->SetValue(dly->getVol());
-		  dist->SetValue(dly->getDistort());
-		feedbk->SetValue(dly->getFeedback());
-		
-		  tune->SetValue((float)(12 + fTrack->getTune())/24.0f);
-		tdelay->SetValue(dly->getTimeDelay() * (1.0f/0.6f));
-		Window()->Unlock();
+	   cutoff->SetValue(fTrack->vcf.getCutoff());
+		 reso->SetValue(fTrack->vcf.getResonance());
+	   envmod->SetValue(fTrack->vcf.getEnvmod());
+		decay->SetValue(fTrack->vcf.getDecay());
+	   	delay->SetValue(fTrack->dly.getVol());
+		 dist->SetValue(fTrack->dly.getDistort());
+	   feedbk->SetValue(fTrack->dly.getFeedback());
+    	 tune->SetValue((float)(12 + fTrack->getTune())/24.0f);
+	   tdelay->SetValue(fTrack->dly.getTimeDelay() * (1.0f/0.6f));
+	  
+	  Window()->Unlock();
 	}
 	
 }
@@ -91,14 +75,14 @@ TB303Knobs::~TB303Knobs()
 }
 
 void TB303Knobs::do_tune(float t)      {  if (fTrack) fTrack->Tune((int)(t*24.0f - 12.0f)); }
-void TB303Knobs::do_cutoff(float c)    {  vcf->Cutoff(c); }
-void TB303Knobs::do_reso(float r)      {  vcf->Resonance(r); }
-void TB303Knobs::do_envmod(float e)    {  vcf->Envmod(e); }
-void TB303Knobs::do_decay(float d)     {  vcf->Decay(d); }
-void TB303Knobs::do_delay(float d)     {  dly->Vol(d); }
-void TB303Knobs::do_feedbk(float d)    {  dly->Feedback(d); }
-void TB303Knobs::do_dist(float d)      {  dly->Distort(d); } //
-void TB303Knobs::do_timedelay(float d) {  dly->TimeDelay(d * 0.6f); } //
+void TB303Knobs::do_cutoff(float c)    {  if (fTrack) fTrack->vcf.Cutoff(c); }
+void TB303Knobs::do_reso(float r)      {  if (fTrack) fTrack->vcf.Resonance(r); }
+void TB303Knobs::do_envmod(float e)    {  if (fTrack) fTrack->vcf.Envmod(e); }
+void TB303Knobs::do_decay(float d)     {  if (fTrack) fTrack->vcf.Decay(d); }
+void TB303Knobs::do_delay(float d)     {  if (fTrack) fTrack->dly.Vol(d); }
+void TB303Knobs::do_feedbk(float d)    {  if (fTrack) fTrack->dly.Feedback(d); }
+void TB303Knobs::do_dist(float d)      {  if (fTrack) fTrack->dly.Distort(d); } //
+void TB303Knobs::do_timedelay(float d) {  if (fTrack) fTrack->dly.TimeDelay(d * 0.6f); } //
 
 void TB303Knobs::AttachedToWindow()
 {
@@ -106,20 +90,6 @@ void TB303Knobs::AttachedToWindow()
 		SetViewColor(Parent()->ViewColor());
 }
 
-APot *
-TB303Knobs::MakeController(BRect r,const char* title,const char* n2,int min,int max ,int cur,const rgb_color col,int32 ms)
-{
-	const BBitmap *x = LoadIcon("tnpot");
-	APot	*p;
-	p = new APot(BRect(r.left,r.top,r.left+POT_H,r.top+POT_H),"",new BMessage(ms),NULL,min,max,(BBitmap*)x,NULL);
-	p->SetTarget(this);
-	p->SetHighColor(col);
-	p->distance = 12;
-	p->SetValue(cur);
-	//AddChild(p);
-	return p;
-}
-typedef void (TB303Knobs::*update_value)(float);
 
 
 TNKnob*
--- src/Extensions/Tn306/knob303.h
+++ src/Extensions/Tn306/knob303.h
@@ -1,50 +1,35 @@
 #ifndef __KNOB303_H
 #define __KNOB303_H
 
-#include "APot.h"
-//#include "seq.h"
 #include "vco303.h"
 #include "vcf303.h"
 #include "vca303.h"
 #include "deverb.h"
 #include <View.h>
-#include <TextControl.h>
 
 class TNTrack;
 class BStringView;
 class TNKnob;
 
 
-
-
 class TB303Knobs: public BView
 {
 public:
+	
 	typedef void (TB303Knobs::*update_value)(float);
+	
+				  TB303Knobs(BRect r);
+				 ~TB303Knobs();
+	void		  Reset(TNTrack*);
 	
 private:
 		
-	//APot  *accent;
 	TNKnob *tune, *dist, *envmod, *decay, *cutoff, *reso, *delay, *feedbk, *tdelay;
 	
 	BStringView *label;
 	
 	TNTrack *fTrack;
-	VCO303 *vco;
-	VCF303 *vcf;
-	VCA303 *vca;
-	Deverb *dly;
-	BTextControl*	name_label;
-
-public:
-			TB303Knobs(BRect r);
-			
-	void		Reset(TNTrack*);
 	
-	 ~TB303Knobs();
-
-
-private:	
 	void do_tune(float t);
 	void do_cutoff(float c);
 	void do_reso(float r);
@@ -55,9 +40,8 @@ private:
 	void do_dist(float d);
 	void do_timedelay(float d);
 
-protected:
+
 	void AttachedToWindow();
-	APot * MakeController(BRect r,const char* n1,const char* n2,int min,int max ,int cur,const rgb_color col,int32);
 	TNKnob* MakeKnob(const char* title, update_value func);
 };
 
