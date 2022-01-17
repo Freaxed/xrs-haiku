f63ad954860f5ec6863d6965b0ab149ce78f295d
Andrea Anzani <andrea.anzani@gmail.com>
2022-01-17 10:35:25 +0100

    New test app to play with vertical peak view

---
+++ src/tests/test-vert-peak/MainWindow.cpp
@@ -0,0 +1,62 @@
+#include "MainWindow.h"
+
+#include <Application.h>
+#include <GridLayout.h>
+#include <LayoutBuilder.h>
+#include <GroupLayout.h>
+#include "VertPeakView.h"
+#include "PeakView.h"
+#include <Button.h>
+#include <MessageRunner.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Box.h>
+
+VertPeakView* peakV;
+PeakView* peakH;
+
+MainWindow::MainWindow(void)
+	:	BWindow(BRect(100,100,500,400),"Main Window",B_TITLED_WINDOW, B_ASYNCHRONOUS_CONTROLS)
+{
+	BGroupLayout* group = BLayoutBuilder::Group<>(B_HORIZONTAL);
+	SetLayout(group);
+	group->AddView(peakV = new VertPeakView("_peak_", false, false));
+	group->AddView(peakH = new     PeakView("_peak_", false, false));
+	peakV->SetPeakNotificationWhat('yyyy');
+	peakH->SetPeakNotificationWhat('yyyy');
+
+	
+	BMessage message('xxxx');
+	new BMessageRunner(BMessenger(this), &message, 20000);
+}
+
+
+void
+MainWindow::MessageReceived(BMessage *msg)
+{
+	switch (msg->what)
+	{
+		case 'xxxx': {
+			BMessage* x = new BMessage('yyyy');
+			x->AddFloat("max", (float)rand()/(float)RAND_MAX);
+			x->AddFloat("max", (float)rand()/(float)RAND_MAX);
+			PostMessage(x, peakV);//->Invalidate();
+			PostMessage(x, peakH);//->Invalidate();
+		}
+		break;
+		default:
+		{
+			BWindow::MessageReceived(msg);
+			break;
+		}
+	}
+}
+
+
+bool
+MainWindow::QuitRequested(void)
+{
+	be_app->PostMessage(B_QUIT_REQUESTED);
+	return true;
+}
