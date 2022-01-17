ba2c96b2946061778185f0d6968b608fc0b97eee
Andrea Anzani <andrea.anzani@gmail.com>
2022-01-17 10:33:45 +0100

    ValuablePeakView can now be Horizontal or Vertical

--- src/GUI/ValuablePeakView.cpp
+++ src/GUI/ValuablePeakView.cpp
@@ -5,36 +5,3 @@
 
 
 #include "ValuablePeakView.h"
-
-ValuablePeakView::ValuablePeakView(ValuableID id, const char* name)
-				 :PeakView(name, false, false), vID(id) {
-
-
-}
-
-void				
-ValuablePeakView::MessageReceived(BMessage* message) {
-		if (message->what == MSG_VALUABLE_CHANGED)
-		{
-			float value_l;
-			float value_r;
-			if (ValuableTools::SearchValues(vID, message, &value_l, &value_r)){
-					SetMax(value_l, 0);
-					SetMax(value_r, 1);			
-			}
-			fGotData = true;
-		}
-	PeakView::MessageReceived(message);
-}
-
-void
-ValuablePeakView::AttachedToWindow(){
-	PeakView::AttachedToWindow();
-	ValuableManager::Get()->RegisterValuableReceiver(vID, this);
-}
-
-void
-ValuablePeakView::DetachedFromWindow(){
-	ValuableManager::Get()->UnregisterValuableReceiver(vID, this);
-	PeakView::DetachedFromWindow();
-}
--- src/GUI/ValuablePeakView.h
+++ src/GUI/ValuablePeakView.h
@@ -8,11 +8,14 @@
 
 #include <SupportDefs.h>
 #include "PeakView.h"
+#include "VertPeakView.h"
 #include "ValuableManager.h"
 
-class ValuablePeakView : public PeakView {
+template<class TYPE>
+class ValuablePeakView : public TYPE {
 public:
-		ValuablePeakView(ValuableID id, const char* name);
+		ValuablePeakView(ValuableID id, const char* name)
+			:TYPE(name, false, false), vID(id) {}
 		
 		void				MessageReceived(BMessage* message);
 		void				AttachedToWindow();
@@ -22,6 +25,38 @@ private:
 		ValuableID	vID;
 
 };
+
+template<class TYPE>
+void ValuablePeakView<TYPE>::AttachedToWindow() {
+	TYPE::AttachedToWindow();
+	ValuableManager::Get()->RegisterValuableReceiver(vID, this);
+}
+
+template<class TYPE>
+void ValuablePeakView<TYPE>::MessageReceived(BMessage* message) {
+		if (message->what == MSG_VALUABLE_CHANGED)
+		{
+			float value_l;
+			float value_r;
+			if (ValuableTools::SearchValues(vID, message, &value_l, &value_r)){
+					TYPE::SetMax(value_l, 0);
+					TYPE::SetMax(value_r, 1);			
+			}
+			TYPE::fGotData = true;
+		}
+	TYPE::MessageReceived(message);
+
+}
+
+template<class TYPE>
+void ValuablePeakView<TYPE>::DetachedFromWindow(){
+	ValuableManager::Get()->UnregisterValuableReceiver(vID, this);
+	TYPE::DetachedFromWindow();
+}
+
+typedef ValuablePeakView<PeakView> 		ValuableHPeakView;
+typedef ValuablePeakView<VertPeakView>  ValuableVPeakView;
+
 
 
 #endif // ValuablePeakView_H
