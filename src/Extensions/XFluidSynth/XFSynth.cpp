3c93dc516b8b9a3128dac25940b794cb7acb0c04
Andrea Anzani <andrea.anzani@gmail.com>
2022-01-27 10:37:08 +0100

    fluidlite logging with xrs logging

--- src/Extensions/XFluidSynth/XFSynth.cpp
+++ src/Extensions/XFluidSynth/XFSynth.cpp
@@ -3,10 +3,31 @@
 #include <Menu.h>
 #include <MenuItem.h>
 
+#include "Log.h"
+
 #define	MAXCHANNEL	16
 
 #define	SYN fSynth.synth
 #define IFS	if(fSynth.synth)
+
+void logFunc(int level, char *message, void *data)
+{
+	switch(level) {
+		case FLUID_PANIC:
+			LogFatal(message);
+		break;
+		case FLUID_ERR:
+			LogError(message);
+		break;
+		case FLUID_WARN:
+			LogInfo(message);
+		break;
+		case FLUID_DBG:
+			LogDebug(message);
+		break;
+	};
+}
+
 
 XFSynth::XFSynth(){
 
@@ -15,6 +36,11 @@ XFSynth::XFSynth(){
 		fSynth.sfont_id=-1;
 
 		settings = NULL;
+		
+		fluid_set_log_function(FLUID_PANIC, logFunc, NULL);
+		fluid_set_log_function(FLUID_ERR,   logFunc, NULL);
+		fluid_set_log_function(FLUID_WARN,  logFunc, NULL);
+		fluid_set_log_function(FLUID_DBG,   logFunc, NULL);
 }
 
 
