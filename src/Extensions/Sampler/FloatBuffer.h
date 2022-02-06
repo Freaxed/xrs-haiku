7038633eee61f677551116ba7af8a4265c9f5614
Andrea Anzani <andrea.anzani@gmail.com>
2022-02-06 18:11:45 +0100

    StaticBuffer now based on new safe class acrivated only on development

---
+++ src/Extensions/Sampler/FloatBuffer.h
@@ -0,0 +1,113 @@
+/*
+ * Copyright by Andrea Anzani <andrea.anzani@gmail.com>
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+#ifndef FloatBuffer_H_
+#define FloatBuffer_H_
+
+#define BOUND_CHECK 0
+
+
+
+#if BOUND_CHECK
+
+class FloatBuffer {
+
+public:
+    ~FloatBuffer()
+    {
+         if (buffer && !wrapped) {
+         	printf("Warning! buffer object destroyed but not freed!\n");
+         	Delete();
+         }
+    }
+
+    float*  buffer;
+    int   	NbFrames;
+    bool	wrapped;
+
+    float test;
+
+    FloatBuffer(): buffer(NULL), NbFrames(0), wrapped(false)
+    {
+    }
+
+    void Create(uint32   frames)
+    {
+        buffer = new float[frames];
+        NbFrames = frames;
+        printf("Created buffer.. %p %d\n", buffer, frames);
+    }
+
+    void Delete()
+    {
+        if (buffer && !wrapped) {
+        	printf("Freeing buffer.. %d\n", NbFrames);
+        	delete[] buffer; 
+        	buffer = NULL; 
+        	wrapped = false;
+        }
+        if (wrapped)
+        	printf("NOT (wrapped) Freeing buffer.. %d\n", NbFrames);
+    }
+    
+    void Wrap(void* data, int frames)
+    {
+    	buffer = (float*)data; 
+    	NbFrames = frames;
+    	wrapped = true;
+    }
+    
+    void Wrap(FloatBuffer& data, int frames)
+    {
+    	buffer = data.buffer; 
+    	NbFrames = frames;
+    	wrapped = true;
+    }
+
+
+	float   operator [](int i) const
+	{
+		if (i < 0 || i >= NbFrames) {
+			printf("Security error: buffer out of bounds! (%d not in 0-%d)", i, NbFrames);
+			debugger("buffer out of bounds!");
+		}
+
+		return buffer[i];
+	}
+
+	float & operator [](int i)
+	{
+		if (i < 0 || i >= NbFrames) {
+			printf("Security error: buffer out of bounds! (%d not in 0-%d)", i, NbFrames);
+			debugger("buffer out of bounds!");
+		}
+
+		return buffer[i];
+	}
+};
+
+#define FloatBufferPtr  FloatBuffer
+#define GET_BUFFER(FBP) FBP.buffer
+#define CREATE_BUFFER(FBP, FRAMES) FBP.Create(FRAMES);
+#define DESTROY_BUFFER(FBP) FBP.Delete(); GET_BUFFER(FBP) = NULL;
+#define WRAP_BUFFER(DATA, FBP, FRAMES) FBP.Wrap(DATA, FRAMES)
+#else
+
+/*
+ * Production version of a FloatBufferPtr = float*
+ */
+
+#define FloatBufferPtr float*
+#define GET_BUFFER(FBP) FBP
+#define CREATE_BUFFER(FBP, FRAMES) FBP = new float[FRAMES]
+#define DESTROY_BUFFER(FBP) delete[] FBP; GET_BUFFER(FBP) = NULL;
+#define WRAP_BUFFER(DATA, FBP, FRAMES) GET_BUFFER(FBP) = (float*)DATA
+#endif
+
+// common functions
+
+#define IS_VALID(FBP) (GET_BUFFER(FBP) != NULL)
+#define SET_INVALID(FBP) GET_BUFFER(FBP) = NULL
+
+#endif //FloatBuffer_H_
--- src/Extensions/Sampler/StaticBuffer.h
+++ src/Extensions/Sampler/StaticBuffer.h
@@ -8,31 +8,43 @@
 
 #include <SupportDefs.h>
 #include "Log.h"
+#include <String.h>
+#include "FloatBuffer.h"
+
 
 template<int NbFrames>
 class StaticBuffer {
-	public:
+
+public:
 	
 	const uint32	kFrames = NbFrames;
 	
-	float*		buffer;
+	FloatBufferPtr		buffer;
 	
-	status_t	Check()  { return buffer ? B_OK : B_ERROR; }
-	float*		Buffer() { return buffer; }
+	status_t	        Check()  { return IS_VALID(buffer) ? B_OK : B_ERROR; }
+	float*		        Buffer() { return GET_BUFFER(buffer); }
+
+
+
 	
-	StaticBuffer() {
-		buffer = (float*)malloc(NbFrames * sizeof (float));				
-		if (buffer == NULL) {
-					 LogError ("MediaFileLoader - Error : Out of memory.\n\n") ;
+	StaticBuffer()
+    {
+        CREATE_BUFFER(buffer, NbFrames);
+		if (!IS_VALID(buffer)) {
+			LogError ("MediaFileLoader - Error : Out of memory.") ;
 		}
-		LogTrace("StaticBuffer ready (%d floats)!", NbFrames);
+		LogTrace("StaticBuffer ready (%d floats) (bound_check %d)!", NbFrames, BOUND_CHECK);
 	}
+
 	~StaticBuffer() {
-		if (buffer) {
+		if (IS_VALID(buffer)) {
 			LogTrace("StaticBuffer destroyed!");
-			free(buffer);
+			DESTROY_BUFFER(buffer);
 		}
 	}
+
+	inline float   operator [](int i) const    { return buffer[i]; }
+    inline float & operator [](int i) 		   { return buffer[i]; }
 };
 
 
