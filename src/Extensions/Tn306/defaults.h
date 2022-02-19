e2f25f9cb3c8477dd30254d8e54e47663c178c51
Andrea Anzani <andrea.anzani@gmail.com>
2022-02-19 15:59:24 +0100

    adding missing file

---
+++ src/Extensions/Tn306/defaults.h
@@ -0,0 +1,16 @@
+#ifndef _DEFAULTS_H_
+#define _DEFAULTS_H_
+
+#define DEF_VCF_CUTOFF 0.0f
+#define DEF_VCF_RESO   0.0f
+#define DEF_VCF_ENVMOD 0.0f
+#define DEF_VCF_DECAY  0.0f
+#define DEF_DLY_VOL	   0.0f
+#define DEF_DLY_DIST   0.50f
+#define DEF_DLY_FBK	   0.0f
+#define DEF_DLY_TIME   0.25f
+#define DEF_TUNE	   0
+
+#define PUMP_UP_FACTOR 3.0f
+
+#endif //_DEFAULTS_H_
